727dbbea41f4f69c14f3067d78c27212626287a9
HubertMurawski <hubert@mwcoders.com>
2017-05-29 20:29:36 +0200

    1

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Nauka Githuba</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		
+	</body>
+</html>
